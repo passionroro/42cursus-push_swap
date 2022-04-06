bd9343459a8417681629df3be41521dfac5d49a1
Romain Hoarau <rohoarau@student.42lausanne.ch>
2022-04-06 13:27:20 +0200

    86/100

---
+++ src/main.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rohoarau <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/10 10:27:54 by rohoarau          #+#    #+#             */
+/*   Updated: 2022/04/05 12:42:36 by rohoarau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/push_swap.h"
+
+int	algorithms(t_tab *t)
+{
+	if (t->pos == 2)
+		return (sa(t));
+	if (t->pos == 3)
+		return (algorithm3(t));
+	if (t->pos >= 4 && t->pos <= 5)
+		return (algorithm5(t));
+	simplified_sort(t, t->a, insertion_sort(t));
+	if (t->pos > 5 && t->pos <= 20)
+		algorithm100(t, 2);
+	if (t->pos > 20 && t->pos <= 100)
+		algorithm100(t, 4);
+	if (t->pos > 100)
+		algorithm100(t, 8);
+	if (t->posb)
+	{
+		pa(t);
+		ra(t);
+		free(t->b);
+	}
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_tab	t;
+
+	if (argc < 2)
+		return (0);
+	if (argc == 2)
+		create_tab(&t, ft_split(argv[1], ' '), argc);
+	if (argc > 2)
+		create_tab(&t, argv, argc);
+	check_errors(&t);
+	algorithms(&t);
+	free(t.a);
+	return (0);
+}
